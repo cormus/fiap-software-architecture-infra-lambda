65b4c399802f1ba1f6a9cb0b7db513b20cef1e7c
Alex Ribeiro <alexprodutor.com@gmail.com>
2025-06-01 14:51:41 -0300

    atualização

--- infra/main.tf
+++ infra/main.tf
@@ -83,7 +83,7 @@ resource "aws_api_gateway_integration" "rest_integration" {
 
   request_templates = {
     "application/json" = <<EOF
-{"cpf": "$input.params('cpf')"}
+{"cpf": $input.params("cpf")}
 EOF
   }
     
