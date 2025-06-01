40b2de6e9ec01246f6a0b1c0b8bb7e0be24fd329
Alex Ribeiro <alexprodutor.com@gmail.com>
2025-06-01 10:45:40 -0300

    Teste de criação de gateway

--- infra/main.tf
+++ infra/main.tf
@@ -21,12 +21,43 @@ resource "aws_iam_policy_attachment" "lambda_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+# -------- Lambda Function --------
+
 resource "aws_lambda_function" "lambda_function" {
-  function_name = "login-lambda-function"
+  function_name = "lanchonete-login-lambda-function"
   role          = aws_iam_role.lambda_role.arn
   handler       = "index.handler"
   runtime       = "nodejs18.x"
 
   source_code_hash = filebase64sha256("../lambda/lambda.zip")
   filename         = "../lambda/lambda.zip"
+}
+
+
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "lambda-api-gateway"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id           = aws_apigatewayv2_api.http_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.lambda_function.arn
+}
+
+resource "aws_apigatewayv2_route" "http_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /{cpf}"
+
+  target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "default"
+  auto_deploy = true
+}
+
+output "api_gateway_url" {
+  value = aws_apigatewayv2_stage.default_stage.invoke_url
 }
